8c6d323367789f1cff297922236b159f75c4d8b0
TheoCib <theophile.cibert@ynov.com>
2018-05-15 11:51:35 +0200

    Projet Fini

--- indexe.js
+++ indexe.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 app.set('view engine', 'pug');
 
@@ -10,8 +11,20 @@ app.get('/',(request, response) => {
 
 app.get('/user/', (req,res) =>{
    const userName = req.query.user;
+    const accesAPI = { url:'https://api.github.com/users/' + userName, headers: {'User-Agent': "StudentProject"}}
    console.log(userName);
-   res.render("user");
+   request(accesAPI, (err,response,body) =>{
+       if (err) {
+           console.error("erreur" + err);
+       } else {
+           // body is a string that needs to be parsed
+           const user = JSON.parse(body);
+           console.log(user);
+           res.render("user" , {user});
+
+       }
+
+   });
 });
 
 app.listen(3000);
